59115f4dde805564e11a2a847cfbfe8bb6ef39c1
d3ric3 <d3ric3@gmail.com>
2021-08-03 23:19:18 +0800

    Progress "4:50:00"

--- react-cars-app/src/app/components/Footer/index.tsx
+++ react-cars-app/src/app/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components/macro";
@@ -6,15 +6,14 @@ import tw from "twin.macro";
 import { Logo } from "../Logo";
 
 const FooterContainer = styled.div`
-  min-height: 24em;
+  //min-height: 24em;
   background-color: #1d2124;
   ${tw`
     flex
     flex-col
     min-w-full
-    pt-4
-    md:pt-12
-    pb-1
+    pt-10
+    md:pt-16
     items-center
     justify-center
   `}
@@ -26,15 +25,40 @@ const InnerContainer = styled.div`
     w-full
     h-full
     max-w-screen-2xl
+    flex-wrap
+  `}
+`;
+
+const BottomContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    max-w-screen-2xl
+    justify-center
+    md:justify-start
+    mt-7
+    md:mt-1
+  `}
+`;
+
+const CopyrightText = styled.small`
+  font-size: 12px;
+  ${tw`
+    text-gray-300
   `}
 `;
 
 const AboutContainer = styled.div`
   ${tw`
+    w-52
     flex
     flex-col
     mr-2
     md:mr-16
+    pl-10
+    pr-10
+    md:pr-3
+    md:pl-3
   `}
 `;
 
@@ -51,10 +75,18 @@ const AboutText = styled.p`
 
 const SectionContainer = styled.div`
   ${tw`
+    w-full
+    md:w-auto
     flex 
     flex-col
     mr-2
     md:mr-16
+    pl-10
+    pr-10
+    md:pr-3
+    md:pl-3
+    mt-7
+    md:mt-0
   `}
 `;
 
@@ -181,7 +213,22 @@ function Footer() {
             <SmallText>+91 555-234-8469</SmallText>
           </HorizontalContainer>
         </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Mail</HeaderTitle>
+          <HorizontalContainer>
+            <RedIcon>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </RedIcon>
+            <SmallText>info@yourcar.com</SmallText>
+          </HorizontalContainer>
+        </SectionContainer>
       </InnerContainer>
+      <BottomContainer>
+        <CopyrightText>
+          Copyright &copy; {new Date().getFullYear()} Yourcar. All rights
+          reserved.
+        </CopyrightText>
+      </BottomContainer>
     </FooterContainer>
   );
 }
