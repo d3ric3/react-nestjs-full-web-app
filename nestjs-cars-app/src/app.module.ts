fb217bf58933c857b17f8a5ee9f2d36a3bd30319
d3ric3 <d3ric3@gmail.com>
2021-08-10 21:37:51 +0800

    Progress "5:47:09"

--- nestjs-cars-app/src/app.module.ts
+++ nestjs-cars-app/src/app.module.ts
@@ -4,15 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
     GraphQLModule.forRoot({
-      debug: (process.env.NODE_ENV === 'development' && true) || false,
-      playground: (process.env.NODE_ENV === 'development' && true) || false,
+      playground: process.env.NODE_ENV === 'development' ? true : false,
+      debug: process.env.NODE_ENV === 'development' ? true : false,
+      autoSchemaFile: true,
     }),
+    ComponentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
